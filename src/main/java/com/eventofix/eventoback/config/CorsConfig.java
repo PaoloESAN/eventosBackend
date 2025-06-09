583689bd7b144672f68a8642c4b3e28b93995f95
Paolo <186249117+PaoloESAN@users.noreply.github.com>
2025-06-09 18:36:53 -0500

    agregado meotodo options al CORS

--- src/main/java/com/eventofix/eventoback/config/CorsConfig.java
+++ src/main/java/com/eventofix/eventoback/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
